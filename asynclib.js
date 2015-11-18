3851b4eb642d18854bce48951c51464b5413a476
Hans Felde <m33pfish@gmail.com>
2015-11-18 15:53:56 -0600

    Update asynclib.js

--- asynclib.js
+++ asynclib.js
@@ -1,7 +1,11 @@
 var async = {};
 async.while = function(cond, func, time) {
-  var f = function(g) {
-    cond() ? setTimeout(() => (func(), g(g)), time || 0) : null;
-  };
+  var broken = false;
+  var f = g =>
+    cond() && !broken ? setTimeout(() => (func(), g(g)), time || 0) : null;
   f(f);
+  return () => broken = true;
 };
+async.forever = (func, time) => async.while(() => true, func, time);
+async.forEach = (arry, func, time, i) => (i = i || 0,
+  async.while(() => i < arry.length, () => (func(arry[i]), i++), time));

120c8c83690e27ca6813976e1e93f6780729e370
Hans Felde <m33pfish@gmail.com>
2015-11-18 16:40:15 -0600

    Update asynclib.js

--- asynclib.js
+++ asynclib.js
@@ -1,11 +1,33 @@
 var async = {};
-async.while = function(cond, func, time) {
+async.while = function(args) {
   var broken = false;
   var f = g =>
-    cond() && !broken ? setTimeout(() => (func(), g(g)), time || 0) : null;
+    args.cond() && !broken ? setTimeout(() => (args.func(), g(g)), args.time || 0) : args.noAftIfBrk ? !broken ? (args.after || (a) => a)() : null : (args.after || (a) => a)();
   f(f);
   return () => broken = true;
 };
-async.forever = (func, time) => async.while(() => true, func, time);
-async.forEach = (arry, func, time, i) => (i = i || 0,
-  async.while(() => i < arry.length, () => (func(arry[i]), i++), time));
+async.forever = (args) => async.while({
+  cond: () => true,
+  func: args.func,
+  after: args.after,
+  time: args.time || 0
+});
+
+async.map = (args, i) => (i = i || 0, async.while({
+  cond: () => i < args.arry.length,
+  func: () => (args.arry[i] = args.func(args.arry[i], i, args.arry), i++),
+  after: args.after,
+  time: args.time || 0
+}));
+async.filter = (args, arry) => 
+  arry = [],
+  async.map({
+    arry: args.arry
+    func: e => args.cond(e) ? arry.push(e) : null
+    after: () => args.after(arry)
+    time: args.time || 0
+  })
+
+//async.chain = function(args) {
+//  
+//};
